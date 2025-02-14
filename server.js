28bf43109af450dccc48be4c9cf7d0fec44f030f
wencaili <228295392@qq.com>
2025-02-14 16:47:25 +0800

    json-server

---
+++ server.js
@@ -0,0 +1,29 @@
+const jsonServer = require('json-server');
+const fs = require('fs');
+const path = require('path');
+
+const groupList = JSON.parse(fs.readFileSync(
+  path.join(__dirname, 'es6/json/preview/question/q7.json'),
+  'utf-8'
+));
+const paperPreview = JSON.parse(fs.readFileSync(
+  path.join(__dirname, 'es6/json/preview/template/t7.json'),
+  'utf-8'
+));
+const mergedDb = {
+  local: {
+    groupList,
+    paperPreview
+  }
+};
+
+const server = jsonServer.create();
+const router = jsonServer.router(mergedDb);
+const middlewares = jsonServer.defaults();
+
+server.use(middlewares);
+server.use(router);
+
+server.listen(6666, () => {
+  console.log('JSON Server is running on port 6666');
+});
